79587238da7a364b07113f4aba99b1108e9775ff
Jonas Krogh <xyzkrgu@gmail.com>
2025-12-03 08:56:03 +0100

    Avoid converting to string

--- src/day03.js
+++ src/day03.js
@@ -10,26 +10,28 @@ let part2 = 0;
 for (const bank of banks) {
     const voltages = bank.split('').map(e => +e);
 
-    let num = "";
+    let num = 0;
     let lastIndex = -1;
     for (let i = 0; i < 2; ++i) {
         const highest = Math.max(...voltages.slice(lastIndex + 1, (-1 + i || undefined)));
         const first = voltages.indexOf(highest, lastIndex + 1);
 
+        num *= 10
         num += highest
         lastIndex = first
     }
-    part1 += Number(num);
-    num = ""
+    part1 += num;
+    num = 0
     lastIndex = -1;
     for (let i = 0; i < 12; ++i) {
         const highest = Math.max(...voltages.slice(lastIndex + 1, (-11 + i || undefined)));
         const first = voltages.indexOf(highest, lastIndex + 1);
 
+        num *= 10
         num += highest
         lastIndex = first
     }
-    part2 += Number(num)
+    part2 += num
 }
 
 console.log(part1)
