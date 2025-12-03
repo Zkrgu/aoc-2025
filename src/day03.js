a3315801c27ab1d8958b9c0a66d049bf8c9282e4
Jonas Krogh <xyzkrgu@gmail.com>
2025-12-03 06:25:08 +0100

    Day 3

---
+++ src/day03.js
@@ -0,0 +1,36 @@
+import { readFileSync } from "node:fs";
+
+const input = readFileSync(process.stdin.fd, "utf-8").trim();
+
+const banks = input.trim().split('\n')
+
+let part1 = 0;
+let part2 = 0;
+
+for (const bank of banks) {
+    const voltages = bank.split('').map(e => +e);
+
+    let num = "";
+    let lastIndex = -1;
+    for (let i = 0; i < 2; ++i) {
+        const highest = Math.max(...voltages.slice(lastIndex + 1, (-1 + i || undefined)));
+        const first = voltages.indexOf(highest, lastIndex + 1);
+
+        num += highest
+        lastIndex = first
+    }
+    part1 += Number(num);
+    num = ""
+    lastIndex = -1;
+    for (let i = 0; i < 12; ++i) {
+        const highest = Math.max(...voltages.slice(lastIndex + 1, (-11 + i || undefined)));
+        const first = voltages.indexOf(highest, lastIndex + 1);
+
+        num += highest
+        lastIndex = first
+    }
+    part2 += Number(num)
+}
+
+console.log(part1)
+console.log(part2)
